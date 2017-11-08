2571eb2f587b9bab42b1dce5574e09991e608daf
pakon <pakon@DomePakornNB>
2017-11-08 20:18:05 +0700

    Update sprint1 newwww

--- cs284Project/src/RunProgram.java
+++ cs284Project/src/RunProgram.java
@@ -1,8 +1,18 @@
 
+import javax.swing.JFrame;
 import View.LoginPage;
+import View.createSubjectPage;
+import View.profresserPage;
 
 public class RunProgram {
 	public static void main(String[] args) {
-		LoginPage login = new LoginPage();
+		//LoginPage login = new LoginPage();
+		JFrame frame = new JFrame();
+		frame.add(new createSubjectPage());
+		frame.pack();
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 }
