0e3a91af6f2f56a3aeaa672d880c0ff8655811ad
pakon <pakon@DomePakornNB>
2017-11-08 18:36:21 +0700

    Update subjectMgnt

--- cs284Project/src/Controller/SubjectMgnt.java
+++ cs284Project/src/Controller/SubjectMgnt.java
@@ -2,10 +2,44 @@ package Controller;
 
 import java.util.ArrayList;
 
-import Model.ExamResult;
+import Model.GradingCriteria;
+import Model.StudentResult;
+import Model.Subject;
+import Model.User;
 
 public class SubjectMgnt {
-	public ArrayList<ExamResult> checkGrading() {
+
+	public ArrayList<StudentResult> checkGrading() { // return นักศึกษาที่คะแนนยังไม่ครบ
 		return null;
+	}
+
+	private boolean dropStudent(String id) {// Coming Soon
+		return false;
+	}
+
+	public ArrayList<Subject> getAllSubject() {
+		ArrayList<Subject> subList = new ArrayList<>();
+		// database
+		return null;
+	}
+
+	public ArrayList<Subject> getMySubject(User user) {
+		if (user.getRank().equals("PROFESSOR")) {
+			ArrayList<Subject> mySub = new ArrayList<>();
+			for (Subject subject : mySub) {
+				if (subject.getOwnerUser().equals(user.getUserName())) {
+					mySub.add(subject);
+				}
+			}
+			if (mySub.size() > 0) {
+				return mySub;
+			}
+		}
+		return null;
+	}
+
+	public boolean editGradingCriteria(GradingCriteria grade, Subject s) {
+		//Database
+		return false;
 	}
 }
