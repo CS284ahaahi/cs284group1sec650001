2d240b28331437a07c2fb49ba4d9c49953f0004d
pakon <pakon@DomePakornNB>
2017-11-20 20:08:20 +0700

    Update xls export (Uncomplete)

--- cs284Project/src/Controller/FileMgnt.java
+++ cs284Project/src/Controller/FileMgnt.java
@@ -10,6 +10,7 @@ import java.sql.Statement;
 
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import Model.Subject;
@@ -18,38 +19,46 @@ public class FileMgnt {
 
 	public static boolean exportExcelGrade(String tableName) {
 		JFileChooser chooser = new JFileChooser();
-		chooser.setFileFilter(new FileNameExtensionFilter("Text File (*.txt)", "txt"));
+		FileFilter fileTxt = new FileNameExtensionFilter("Text File (*.txt)", "txt");
+		FileFilter fileExcel = new FileNameExtensionFilter("Excel File (*.xls)", "xls");
+		chooser.setFileFilter(fileTxt);
+		chooser.addChoosableFileFilter(fileExcel);
 		String sql = "select * from " + tableName + " Where STATUS like '%N%'";
 		int check = chooser.showSaveDialog(null);
 		if (check == JFileChooser.APPROVE_OPTION) {
 			ResultSet rs;
 			File file = chooser.getSelectedFile();
-			file = new File(file.getAbsolutePath() + ".txt");
-			try (PrintWriter write = new PrintWriter(file);) {
-				Connection con = ConnectMgnt.getConnect();
-				Statement st = con.createStatement();
-				rs = st.executeQuery(sql);
-				write.println("เลขทะเบียนนักศึกษา\tเกรด");
-				while (rs.next()) {
-					String str = rs.getString("ID_STUDENT") + "\t";
-					str += rs.getString("GRADE");
-					write.println(str);
+			if (chooser.getFileFilter().equals(fileTxt)) { // txt
+				file = new File(file.getAbsolutePath() + ".txt");
+				try (PrintWriter write = new PrintWriter(file);) {
+					Connection con = ConnectMgnt.getConnect();
+					Statement st = con.createStatement();
+					rs = st.executeQuery(sql);
+					write.println("เลขทะเบียนนักศึกษา\tเกรด");
+					while (rs.next()) {
+						String str = rs.getString("ID_STUDENT") + "\t";
+						str += rs.getString("GRADE");
+						write.println(str);
+					}
+					return true;
+				} catch (SQLException e) {
+					JOptionPane.showMessageDialog(null, "Database Error.!!!", "ERROR", JOptionPane.ERROR_MESSAGE);
+				} catch (IOException e) {
+					JOptionPane.showMessageDialog(null, e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
 				}
-				return true;
-			} catch (SQLException e) {
-				JOptionPane.showMessageDialog(null, "Database Error.!!!", "ERROR", JOptionPane.ERROR_MESSAGE);
-			} catch (IOException e) {
-				JOptionPane.showMessageDialog(null, e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+			} else { // xls
+				file = new File(file.getAbsolutePath() + ".xls");
+				try {
+					Connection con = ConnectMgnt.getConnect();
+					Statement st = con.createStatement();
+					rs = st.executeQuery(sql);
+					return true;
+				} catch (SQLException e) {
+					JOptionPane.showMessageDialog(null, "Database Error.!!!", "ERROR", JOptionPane.ERROR_MESSAGE);
+				}
 			}
 		}
 		return false;
 	}
-
-	// public static void main(String[] args) {
-	// Subject sub = new Subject("Test", "CS284", "650001", "test", "1", "2560");
-	// if(exportExcelGrade(sub)) {
-	// JOptionPane.showMessageDialog(null, "Export เสร็จสิ้น");
-	// }
-	// }
 
 }
--- cs284Project/src/RunProgram.java
+++ cs284Project/src/RunProgram.java
@@ -5,7 +5,7 @@ import View.LoginPage;
 
 public class RunProgram {
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "นี่คือ version demo ของ sprint1");
+		//JOptionPane.showMessageDialog(null, "นี่คือ version demo ของ sprint1");
 		LoginPage login = new LoginPage();
 	}
 }
