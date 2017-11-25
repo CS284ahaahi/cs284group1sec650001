f10bead4fc07581a2ff7d8606913447067ac207e
PotsathonNB <PotsathonNB@DESKTOP-63GST3D>
2017-11-26 04:15:09 +0700

    add tesr grading

--- cs284Project/test/testcheckgrade.java
+++ cs284Project/test/testcheckgrade.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import Controller.SubjectMgnt;
+import Model.ExamCriteria;
 import Model.ExamResult;
 import Model.GradingCriteria;
 import Model.StudentResult;
@@ -11,18 +12,31 @@ import Model.Subject;
 
 public class testcheckgrade {
 
-	SubjectMgnt s = new  SubjectMgnt();
-	
+	Subject s;
+	ExamResult er;
+	ExamCriteria ec;
 	@Before
 	public void setUp() throws Exception {
-		//s = new Subject(1, "วิศวกรรมซอฟต์แวร์เบื้องต้น", "Intro. to Software Engineering", "CS284", "650001", "Owner", "1","2560", null, null, new GradingCriteria(1,95,75,70,65,60,55,50), null,null);
+		er = new ExamResult(1);
+		er.addStudentResult(new StudentResult(1,"5909650029",20, 35,new double[] {5,5,5,5,5},"-","N"));
+		er.addStudentResult(new StudentResult(1,"5909650185", 0, 0,new double[] {0,0,0,0,0},"-", "W"));
+		er.addStudentResult(new StudentResult(1,"5909650219", 25, 40,new double[] {5,5,5,5,5},"-", "N"));
+		er.addStudentResult(new StudentResult(1,"5909650185", 20, 20,new double[] {5,5,5,5,5},"-", "N"));
+		er.addStudentResult(new StudentResult(1,"5909650185", 0, 0,new double[] {0,0,0,0,0},"-", "W"));
+		ec = new ExamCriteria(0, 5);
+		ec.setFinalFull(50);
+		ec.setScorePer(new int[] {5,5,5,5,5 });
+		ec.setScore(new int[] {5,5,5,5,5 });
+		ec.setFinalPer(50);
+		ec.setMidPer(25);
+		ec.setMidFull(20);
+		s = new Subject(1, "วิศวกรรมซอฟต์แวร์เบื้องต้น", "Intro. to Software Engineering", "CS284", "650001", "Owner", "1","2560", null, er, new GradingCriteria(1,95,75,70,65,60,55,50), ec,null);
 	}
 	
 	@Test
 	public void test() 
 	{
-		
-		assertNotNull(SubjectMgnt.checkGrading(59));
+		assertTrue(SubjectMgnt.gradingExam(s));
 	}
 
 }
